ec16539274eea7baababee8a638da5e520481457
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-15 13:24:24 +0200

    Add firebase configuration file

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2h26XL49hG9k0CLbUPEnOiOQOGCtpGCg',
+  authDomain: 'gardener-82156.firebaseapp.com',
+  projectId: 'gardener-82156',
+  storageBucket: 'gardener-82156.appspot.com',
+  messagingSenderId: '954357204616',
+  appId: '1:954357204616:web:7441211d8ce8c16bd2813d',
+}
+
+export const app = initializeApp(firebaseConfig)
