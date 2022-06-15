e243b0c7923030131c7b179fbe10978afc0bce71
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-15 14:25:01 +0200

    Add react router

--- src/Providers.js
+++ src/Providers.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
 import { GlobalStyle } from 'styles/GlobalStyle'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'styles/theme'
+
+import { BrowserRouter } from 'react-router-dom'
+
 import { Provider } from 'react-redux'
 import store from './store'
 
 const Providers = ({ children }) => (
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </BrowserRouter>
   </Provider>
 )
 
