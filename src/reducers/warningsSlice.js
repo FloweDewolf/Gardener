d4397e93b417c9d81ba618addb60039f17a24750
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-16 20:48:32 +0200

    Add warningSlice

--- src/reducers/warningsSlice.js
+++ src/reducers/warningsSlice.js
@@ -3,19 +3,30 @@ import { createSlice } from '@reduxjs/toolkit'
 const warningsSlice = createSlice({
   name: 'warnings',
   initialState: {
-    value: [
-      {
-        title: 'title',
-        message: 'message',
-      },
-    ],
+    value: {
+      formValues: { title: '', message: '' },
+      warnings: [],
+    },
   },
   reducers: {
+    changeFormValues: (state, action) => {
+      state.value.formValues[action.payload.id] = action.payload.value
+    },
+    clearForm: (state) => {
+      state.value.formValues.title = ''
+      state.value.formValues.message = ''
+    },
     addWarning: (state) => {
-      state.value = [...state.value, {}]
+      state.value.warnings = [
+        ...state.value.warnings,
+        {
+          title: state.value.formValues.title,
+          message: state.value.formValues.message,
+        },
+      ]
     },
   },
 })
 
-export const { addWarning } = warningsSlice.actions
+export const { changeFormValues, clearForm, addWarning } = warningsSlice.actions
 export default warningsSlice.reducer
