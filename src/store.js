3f249c7e7cccbb8d9f268629bc90838c9d613d60
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-16 19:41:02 +0200

    Add warnings to the store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from 'reducers/authSlice'
+import warningsReducer from 'reducers/warningsSlice'
 
 export default configureStore({
   reducer: {
     auth: authReducer,
+    warnings: warningsReducer,
   },
 })
