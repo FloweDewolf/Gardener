b32a717810e9c81f5dc962400757ede9c14e319b
Mateusz Wilk <business.mateusz.wilk@gmail.com>
2022-06-20 12:28:33 +0200

    Handle an error that occurs when rendering a Weather view component

--- src/views/Weather.js
+++ src/views/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setLocation } from 'slices/locationReducer'
+import { useNavigate } from 'react-router-dom'
 
 import { Container, SearchWrapper } from './Weather.styles'
 
@@ -28,36 +29,41 @@ const Weather = () => {
     setCity(e.target.value)
   }
 
-  return (
-    <Container>
-      <SearchWrapper>
-        <h2>Weather in</h2>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input
-              placeholder={weather.name}
-              type="text"
-              onChange={handleChangeInWeather}
-              value={city}
-            />
-            <button type="submit">🔍</button>
-          </form>
-        </div>
-      </SearchWrapper>
-      <p>Feels like {Math.floor(weather.main.feels_like)}°C</p>
-      <p>Min {Math.floor(weather.main.temp_min)}°C</p>
-      <p>Max {Math.floor(weather.main.temp_max)}°C</p>
-      <p>Pressure {weather.main.pressure} hPa</p>
-      <p>Wind speed {Math.floor(weather.wind.speed * 3.6)} km/h</p>
-      <p>Humidity {weather.main.humidity}%</p>
-      <img
-        src={`http://openweathermap.org/img/wn/${`${String(
-          weather.weather[0].icon
-        ).slice(0, -1)}n`}@4x.png`}
-        alt=""
-      />
-    </Container>
-  )
+  try {
+    return (
+      <Container>
+        <SearchWrapper>
+          <h2>Weather in</h2>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                placeholder={weather.name}
+                type="text"
+                onChange={handleChangeInWeather}
+                value={city}
+              />
+              <button type="submit">🔍</button>
+            </form>
+          </div>
+        </SearchWrapper>
+        <p>Feels like {Math.floor(weather.main.feels_like)}°C</p>
+        <p>Min {Math.floor(weather.main.temp_min)}°C</p>
+        <p>Max {Math.floor(weather.main.temp_max)}°C</p>
+        <p>Pressure {weather.main.pressure} hPa</p>
+        <p>Wind speed {Math.floor(weather.wind.speed * 3.6)} km/h</p>
+        <p>Humidity {weather.main.humidity}%</p>
+        <img
+          src={`http://openweathermap.org/img/wn/${`${String(
+            weather.weather[0].icon
+          ).slice(0, -1)}n`}@4x.png`}
+          alt=""
+        />
+      </Container>
+    )
+  } catch (err) {
+    const navigate = useNavigate()
+    return navigate('home')
+  }
 }
 
 export default Weather
